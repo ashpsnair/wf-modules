5dc508eef743099793b6056703b174d0dc4fc824
Aiswarya P.S <69599173+ashpsnair@users.noreply.github.com>
2025-08-21 22:44:42 +0800

    sep-of-function

--- sep-of-function/slice-bam.sh
+++ sep-of-function/slice-bam.sh
@@ -1,3 +1,9 @@
+##################### --------------------------------------------- #####################
+# RAD51_S181P
+##################### --------------------------------------------- #####################
+
+#!/bin/bash
+
 #PBS -l select=1:ncpus=128:mem=256g
 #PBS -l walltime=04:00:00
 #PBS -P 11003581
@@ -29,5 +35,129 @@ find "$BASE_INPUT_DIR" -type f -name "*.cram" | while read -r cramfile; do
   # Extract region, keep header, sort just to be safe, then index
   samtools view -@ 16 -T "$REFERENCE" -b -h "$cramfile" "$CHR" \
     | samtools sort -@ 16 -o "$outbam" -
+  samtools index -@ 16 "$outbam"
+done
+
+
+##################### --------------------------------------------- #####################
+# BRCA2_P3292L
+##################### --------------------------------------------- #####################
+
+#!/bin/bash
+
+#PBS -l select=1:ncpus=128:mem=256g
+#PBS -l walltime=04:00:00
+#PBS -P 11003581
+#PBS -N slice-bam-brca2
+#PBS -j oe
+
+cd $PBS_O_WORKDIR
+module load samtools
+
+REFERENCE="/home/project/11003581/Ref/sarek-refs/Homo_sapiens/GATK/GRCh38/Sequence/WholeGenomeFasta/Homo_sapiens_assembly38.fasta"
+BASE_INPUT_DIR="/home/users/nus/ash.ps/scratch/sep-fun-analysis/BRCA2_P3292L/preprocessing/recalibrated"
+OUTPUT_DIR="/home/users/nus/ash.ps/scratch/sep-fun-analysis/BRCA2_P3292L/preprocessing/chr13-bams"
+mkdir -p "$OUTPUT_DIR"
+
+find "$BASE_INPUT_DIR" -type f -name "*.cram" | while read -r cramfile; do
+  filename=$(basename "$cramfile" .cram)
+  outbam="$OUTPUT_DIR/${filename}_chr13.bam"
+
+  # Detect whether contigs are '13' or 'chr13'
+  if samtools idxstats "$cramfile" | cut -f1 | grep -qx "chr13"; then
+    CHR="chr13"
+  elif samtools idxstats "$cramfile" | cut -f1 | grep -qx "13"; then
+    CHR="13"
+  else
+    echo "Could not find chromosome 13 name in $cramfile"; continue
+  fi
+
+  echo "Processing $cramfile -> $outbam ($CHR)"
+  # Extract region, keep header, sort just to be safe, then index
+  samtools view -@ 16 -T "$REFERENCE" -b -h "$cramfile" "$CHR" \
+    | samtools sort -@ 32 -o "$outbam" -
+  samtools index -@ 16 "$outbam"
+done
+
+
+##################### --------------------------------------------- #####################
+# BRCA1_R133C
+##################### --------------------------------------------- #####################
+
+#!/bin/bash
+
+#PBS -l select=1:ncpus=128:mem=256g
+#PBS -l walltime=04:00:00
+#PBS -P 11003581
+#PBS -N slice-bam-brca1
+#PBS -j oe
+
+cd $PBS_O_WORKDIR
+module load samtools
+
+REFERENCE="/home/project/11003581/Ref/sarek-refs/Homo_sapiens/GATK/GRCh38/Sequence/WholeGenomeFasta/Homo_sapiens_assembly38.fasta"
+BASE_INPUT_DIR="/home/users/nus/ash.ps/scratch/sep-fun-analysis/BRCA1_R133C/preprocessing/recalibrated"
+OUTPUT_DIR="/home/users/nus/ash.ps/scratch/sep-fun-analysis/BRCA1_R133C/preprocessing/chr17-bams"
+mkdir -p "$OUTPUT_DIR"
+
+find "$BASE_INPUT_DIR" -type f -name "*.cram" | while read -r cramfile; do
+  filename=$(basename "$cramfile" .cram)
+  outbam="$OUTPUT_DIR/${filename}_chr17.bam"
+
+  # Detect whether contigs are '17' or 'chr17'
+  if samtools idxstats "$cramfile" | cut -f1 | grep -qx "chr17"; then
+    CHR="chr17"
+  elif samtools idxstats "$cramfile" | cut -f1 | grep -qx "17"; then
+    CHR="17"
+  else
+    echo "Could not find chromosome 17 name in $cramfile"; continue
+  fi
+
+  echo "Processing $cramfile -> $outbam ($CHR)"
+  # Extract region, keep header, sort just to be safe, then index
+  samtools view -@ 16 -T "$REFERENCE" -b -h "$cramfile" "$CHR" \
+    | samtools sort -@ 32 -o "$outbam" -
+  samtools index -@ 16 "$outbam"
+done
+
+
+
+##################### --------------------------------------------- #####################
+# H2A_KO
+##################### --------------------------------------------- #####################
+
+#!/bin/bash
+
+#PBS -l select=1:ncpus=128:mem=256g
+#PBS -l walltime=04:00:00
+#PBS -P 11003581
+#PBS -N slice-bam-brca1
+#PBS -j oe
+
+cd $PBS_O_WORKDIR
+module load samtools
+
+REFERENCE="/home/project/11003581/Ref/sarek-refs/Homo_sapiens/GATK/GRCh38/Sequence/WholeGenomeFasta/Homo_sapiens_assembly38.fasta"
+BASE_INPUT_DIR="/home/users/nus/ash.ps/scratch/sep-fun-analysis/H2A_KO/preprocessing/recalibrated"
+OUTPUT_DIR="/home/users/nus/ash.ps/scratch/sep-fun-analysis/H2A_KO/preprocessing/chr11-bams"
+mkdir -p "$OUTPUT_DIR"
+
+find "$BASE_INPUT_DIR" -type f -name "*.cram" | while read -r cramfile; do
+  filename=$(basename "$cramfile" .cram)
+  outbam="$OUTPUT_DIR/${filename}_chr11.bam"
+
+  # Detect whether contigs are '11' or 'chr11'
+  if samtools idxstats "$cramfile" | cut -f1 | grep -qx "chr11"; then
+    CHR="chr11"
+  elif samtools idxstats "$cramfile" | cut -f1 | grep -qx "11"; then
+    CHR="11"
+  else
+    echo "Could not find chromosome 11 name in $cramfile"; continue
+  fi
+
+  echo "Processing $cramfile -> $outbam ($CHR)"
+  # Extract region, keep header, sort just to be safe, then index
+  samtools view -@ 16 -T "$REFERENCE" -b -h "$cramfile" "$CHR" \
+    | samtools sort -@ 32 -o "$outbam" -
   samtools index -@ 16 "$outbam"
 done
